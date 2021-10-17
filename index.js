2f2b7c92f10c9b1a204b64b1a7670c9f1a1ae118
Cesniux <cesniux@gmail.com>
2021-10-17 22:40:55 +0300

    Test export works

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ var track = new MidiWriter.Track();
 // Assign an instrument to a track
 track.addEvent(new MidiWriter.ProgramChangeEvent({instrument : 1}));
 
-track.addEvent(new MidiWriter.NoteEvent({pitch: ['Bb4'], duration: '2', velocity:100}));
+track.addEvent([
+    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
+    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
+    new MidiWriter.NoteEvent({pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8'}),
+    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'})
+], function(event, index) {
+return {sequential: true};
+}
+);
 
 // Generate MIDI
 var write = new MidiWriter.Writer([track]);
@@ -19,4 +30,4 @@ var data = base64String.split("base64,").pop();
 
 const fs = require('fs');
 // Write the file
-fs.writeFileSync("C:/Cesniux/Codes/MyNodeApps/chess_to_midi/MIDI exports/zelda.mid", data, {encoding: 'base64'});
+fs.writeFileSync("C:/Cesniux/Codes/MyNodeApps/chess_to_midi/MIDI exports/zeldaTest2.mid", data, {encoding: 'base64'});
