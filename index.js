9469c2918d5944992c97ce7fd7195cb4bfa9aced
Cesniux <cesniux@gmail.com>
2021-10-17 22:34:06 +0300

    checking if it can export midi file

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 var MidiWriter = require('midi-writer-js');
 
+// Create Track
 var track = new MidiWriter.Track();
 
-track.addEvent([
-		new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-		new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-		new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-		new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-		new MidiWriter.NoteEvent({pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8'}),
-		new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-		new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'})
-	], function(event, index) {
-    return {sequential: true};
-}
-);
+// Assign an instrument to a track
+track.addEvent(new MidiWriter.ProgramChangeEvent({instrument : 1}));
 
-var write = new MidiWriter.Writer(track);
-console.log(write.dataUri());
+track.addEvent(new MidiWriter.NoteEvent({pitch: ['Bb4'], duration: '2', velocity:100}));
 
-var voice = create_4_4_voice().addTickables(notes);
+// Generate MIDI
+var write = new MidiWriter.Writer([track]);
 
-var vexWriter = new MidiWriter.VexFlow();
-var track = vexWriter.trackFromVoice(voice);
-var writer = new MidiWriter.Writer([track]);
-console.log(writer.dataUri());
+// Generate a base64 representation of the MIDI file
+var base64String = write.base64();
+
+// Strip off the header
+var data = base64String.split("base64,").pop();
+
+const fs = require('fs');
+// Write the file
+fs.writeFileSync("C:/Cesniux/Codes/MyNodeApps/chess_to_midi/MIDI exports/zelda.mid", data, {encoding: 'base64'});
