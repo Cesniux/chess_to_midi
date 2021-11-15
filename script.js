2dfc931a890025f9fa18a20ba34eab37a38032f4
Cesniux <cesniux@gmail.com>
2021-11-16 00:46:55 +0200

    Got full one midi file from all moves, except black castling

--- script.js
+++ script.js
@@ -36,29 +36,64 @@ track.addEvent(new MidiWriter.ProgramChangeEvent({instrument : 1}));
 
 
 // A function to call when white player has element O-O
-var castlingWhiteKing = ['F2', 'G2'];
+var testArr2 = ['E5', 'E6', 'F4', 'C7', 'B6', 'F7', 'D4', 'C6', 'O-O', 'A7', 'A5', 'B6', 'B4', 'D6', 'D6', 'D6', 'C4', 'C4', 'C4', 'G5', 'D3', 'F4', 'F4', 'F7', 'F5', 'F5', 'F5', 'G7', 'G4', 'O-O-O', 'G7', 'G7', 'F8', 'F9', 'G7', 'E6', 'E5', 'G5', 'E4', 'E4', 'E4', 'F2', 'F2', 'E4', 'F3', 'G5', 'G3', 'B3', 'B3', 'B3', 'B3', 'D8', 'G4', 'E7', 'G5', 'F7', 'B6', 'G7', 'G7', 'E6', 'E5', 'C6', 'G5', 'C5', 'F4', 'D4', 'D4', 'A6', 'E4', 'B5', 'B5', 'B5', 'D5', 'D7', 'D4', 'C8', 'C5', 'B7', 'B5', 'C8', 'A5', 'D7', 'A6', 'C8', 'C6', 'D8', 'A7', 'C8', 'D6', 'B9', 'B7', 'C9', 'A8', 'D8', 'A9', 'D7', 'C7', 'E6', 'E7', 'D5'];
+var castlingWhiteKingS = ['F2', 'G2'];
+var castlingWhiteQueenS = ['C2', 'D2'];
+var castlingBlackKingS = ['G9', 'F9'];
+var castlingBlackQueenS = ['C9', 'D9'];
+
 var sequentialVariable1 = false;
-function addingEvent (castlingWhiteKing, sequentialVariable1) { 
-    track.addEvent(new MidiWriter.NoteEvent({pitch: castlingWhiteKing , duration: '2', velocity:100, }),
+
+// adding notes of castling White King
+function addingEvent (testArr2, i) { 
+    track.addEvent(new MidiWriter.NoteEvent({pitch: [testArr2[i]] , duration: '2', velocity:100, }),
     function(event, index) {
-    return {sequential: sequentialVariable1};
+    return {sequential: false};
+    }
+    );
+};
+//addingEvent ();
+function addingEventCastleWK () { 
+    track.addEvent(new MidiWriter.NoteEvent({pitch: ['F2', 'G2'] , duration: '2', velocity:100, }),
+    function(event, index) {
+    return {sequential: false};
+    }
+    );
+};
+function addingEventCastleWQ () { 
+    track.addEvent(new MidiWriter.NoteEvent({pitch: ['C2', 'D2'] , duration: '2', velocity:100, }),
+    function(event, index) {
+    return {sequential: false};
     }
     );
 };
 
 
-var testArr2 = ['E5', 'E6', 'F4', 'C7', 'B6', 'F7', 'D4', 'C6', 'O-O', 'A7', 'A5', 'B6', 'B4', 'D6', 'D6', 'D6', 'C4', 'C4', 'C4', 'G5', 'D3', 'F4', 'F4', 'F7', 'F5', 'F5', 'F5', 'G7', 'G4', 'O-O-O', 'G7', 'G7', 'F8', 'F9', 'G7', 'E6', 'E5', 'G5', 'E4', 'E4', 'E4', 'F2', 'F2', 'E4', 'F3', 'G5', 'G3', 'B3', 'B3', 'B3', 'B3', 'D8', 'G4', 'E7', 'G5', 'F7', 'B6', 'G7', 'G7', 'E6', 'E5', 'C6', 'G5', 'C5', 'F4', 'D4', 'D4', 'A6', 'E4', 'B5', 'B5', 'B5', 'D5', 'D7', 'D4', 'C8', 'C5', 'B7', 'B5', 'C8', 'A5', 'D7', 'A6', 'C8', 'C6', 'D8', 'A7', 'C8', 'D6', 'B9', 'B7', 'C9', 'A8', 'D8', 'A9', 'D7', 'C7', 'E6', 'E7', 'D5'];
+
 //console.log(testArr2);
-addingEvent (castlingWhiteKing);
+//addingEvent (castlingWhiteKingS);
 
-for ( let i = 0; i < testArr2.length; i++ ) {
+// for ( let i = 0; i < testArr2.length; i++ ) {
 
-    // i who does  i%2 == 0 are white's moves
-    if ( testArr2[i] === ('O-O') && i%2 == 0) {
-    console.log(i);
-    addingEvent (castlingWhiteKing);
-    };
-};
+//     // i who does  i%2 == 0 are white's moves
+//     if ( testArr2[i] === ('O-O') && i%2 == 0) {
+//     console.log(i);
+//     addingEventCastleWK ();
+//     }
+//     else if ( testArr2[i] === ('O-O') && i%2 == 0) {
+//     console.log(i);
+//     addingEventCastleWK ();
+//     }
+//     // White queen conditional
+//     else if ( testArr2[i] === ('O-O-O') && i%2 == 0) {
+//         console.log(i);
+//         addingEventCastleWQ ();
+//     }
+//     else {
+//         console.log(i);
+//         addingEvent (testArr2, i);
+//     };
+// };
 
 
 // Generate MIDI yeah
